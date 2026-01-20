3b71fd7504f4d37c2fdea8c9bd5a877bd3a9884c
m.jahangiri.yaghooti <m.jahangiri.yaghooti@accenture.com>
2026-01-20 19:55:09 +0100

    Initial commit - Java assignment

---
+++ src/main/java/Main/helloworld.java
@@ -0,0 +1,7 @@
+package Main;
+
+public class helloworld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ src/main/java/Main/multiplication.java
@@ -0,0 +1,7 @@
+package Main;
+
+public class multiplication {
+    public static void main(String[] args) {
+
+    }
+}
