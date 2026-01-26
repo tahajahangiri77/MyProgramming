98ec73e7bd0f57f6d158bf5a088f95faf306cfa2
m.jahangiri.yaghooti <m.jahangiri.yaghooti@accenture.com>
2026-01-26 20:26:28 +0100

    complete earthquake categorization and safety message

--- src/main/java/Week_3/earthquake.java
+++ src/main/java/Week_3/earthquake.java
@@ -18,8 +18,15 @@ public class earthquake {
                 (magnitude < 6.0) ? "Moderate" :
                 (magnitude < 7.0) ? "Strong" :
                 (magnitude < 8.0) ? "Major" : "Great" ;
-
-
+        switch (category){
+            case "Micro" -> System.out.println("Micro: No action needed; usually not felt.");
+            case "Minor" -> System.out.println("Minor: Slight shaking; stay aware.");
+            case "Light" -> System.out.println("Light:Drop, Cover, and Hold On; minor damage possible.");
+            case "Moderate" -> System.out.println("Moderate: Protect yourself; expect aftershocks.");
+            case "Strong" -> System.out.println("Strong: Serious damage possible; avoid hazards.");
+            case "Major" -> System.out.println("Major: Severe damage expected; follow emergency instructions.");
+            case "Great" -> System.out.println("Great: Catastrophic event; seek immediate safety and official guidance.");
 
     }
+}
 }
