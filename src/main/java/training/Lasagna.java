7bc5b67835464a9442bbd60d8d6264f62c0a0cb0
m.jahangiri.yaghooti <m.jahangiri.yaghooti@accenture.com>
2026-01-26 19:48:43 +0100

    completing the training

--- src/main/java/training/Lasagna.java
+++ src/main/java/training/Lasagna.java
@@ -1,20 +1,20 @@
 package training;
 
 public class Lasagna {
-    // TODO: define the 'expectedMinutesInOven()' method
     public int expectedMinutesInOven(){
+
         return 40;
     }
-    // TODO: define the 'remainingMinutesInOven()' method
     public int remainingMinutesInOven(int actueltime){
+
         return expectedMinutesInOven() - actueltime;
     }
-    // TODO: define the 'preparationTimeInMinutes()' method
     public int preparationTimeInMinutes(int layer){
+
         return layer * 2;
     }
 
-    // TODO: define the 'totalTimeInMinutes()' method
+
     public int totalTimeInMinutes(int x, int y){
         return preparationTimeInMinutes(x) + y;
     }
@@ -26,6 +26,5 @@ public class Lasagna {
         System.out.println(lasagna.preparationTimeInMinutes(2));
 
         System.out.println(lasagna.totalTimeInMinutes(2, 20));
-// => 10
     }
 }
