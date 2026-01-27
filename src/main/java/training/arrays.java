d5fd2f9836ca0dae7d9091f31b6ad045af4dd660
m.jahangiri.yaghooti <m.jahangiri.yaghooti@accenture.com>
2026-01-27 18:01:59 +0100

    completing the array excerise in Udacity

---
+++ src/main/java/training/arrays.java
@@ -0,0 +1,20 @@
+package training;
+
+import java.util.Arrays;
+
+public class arrays {
+    public static void main(String[] args) {
+        int [] numbers = {1,2,3,4,5};
+        String[] words = {"Ignition sequence start!", "Liftoff!"};
+
+        System.out.println(words[0]);
+        System.out.println(numbers[4]);
+        System.out.println(numbers[3]);
+        System.out.println(numbers[2]);
+        System.out.println(numbers[1]);
+        System.out.println(numbers[0]);
+        System.out.println(words[1]);
+
+
+    }
+}
