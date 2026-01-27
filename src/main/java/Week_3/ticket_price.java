68df2cc8264b0158faa9a3d2b402f38fac588b01
m.jahangiri.yaghooti <m.jahangiri.yaghooti@accenture.com>
2026-01-27 17:40:19 +0100

    feat: finalize ticket_price class with conditional discount checks

--- src/main/java/Week_3/ticket_price.java
+++ src/main/java/Week_3/ticket_price.java
@@ -1,23 +1,31 @@
 package Week_3;
+
 import java.util.Scanner;
+
 public class ticket_price {
     public static void main(String[] args) {
         Scanner isStudent = new Scanner(System.in);
         System.out.println("Are you a student? true or false");
 
         Boolean student = isStudent.nextBoolean();
+
         Scanner age = new Scanner(System.in);
         System.out.println("How old are you?");
         int student_age = age.nextInt();
+
         int original_price = 30;
-        int discount = 15;
+
         if (student) {
             int discount_price = original_price / 2;
             System.out.println("The price you have to pay: " + discount_price);
-        } else if (student_age < 15 || student_age > 65) {
+        }
+        else if (student_age < 15 || student_age > 65) {
             int discount_price = original_price / 2;
-        } else {
+            System.out.println("The price you have to pay: " + discount_price);
+        }
+        else {
             int discount_price = original_price;
+            System.out.println("The price you have to pay: " + discount_price);
 
         }
     }
