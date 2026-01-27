95c744ec314e149497ed3821fd6791d52caa0d81
m.jahangiri.yaghooti <m.jahangiri.yaghooti@accenture.com>
2026-01-27 17:47:00 +0100

    feat: finalize the conditions for the grade calculator

--- src/main/java/Week_3/grade_calculator.java
+++ src/main/java/Week_3/grade_calculator.java
@@ -1,20 +1,25 @@
-package Main;
+package Week_3;
+
+import java.util.Scanner;
 
 public class grade_calculator {
-    public static Object main(String[] args) {
-        int score = 90;
-        switch (score / 10) {
-            case 10:
-            case 9:
-                return "A";
-            case 8:
-                return "B";
-            case 7:
-                return "C";
-            case 6:
-                return "D";
-            default:
-                return "F";
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your score: ");
+        int score = input.nextInt();
+        if (score >= 90){
+            System.out.println("Grade A");
+        } else if (score >= 80) {
+            System.out.println("Grade B");
+        } else if (score >= 70) {
+            System.out.println("Grade C");
+        } else if (score >= 60) {
+            System.out.println("Grade D");
+        } else {
+            System.out.println("Grade F");
         }
+
     }
+
 }
+
